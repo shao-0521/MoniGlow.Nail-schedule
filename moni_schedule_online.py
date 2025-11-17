3a62f5ed4fcc78f286ba34009e929f1cad3df567
shao-0521 <wl01690329@gmail.com>
2025-11-17 09:41:09 +0800

    Update moni_schedule_online.py

--- moni_schedule_online.py
+++ moni_schedule_online.py
@@ -150,6 +150,7 @@ def generate_schedule(year, month):
         d = f"{day:02d}"
         choice = st.session_state.get(f"choice_{year}_{month}_{day}", "Time-1")
 
+
         if choice == "None":
             line = f"{month}/{d} ({wd})"
         elif choice == "Dayoff":
